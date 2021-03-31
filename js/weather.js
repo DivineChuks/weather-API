ceb0f279560f3945113698f9eeb32e23b5a70b5e
Divine <chuksbon6@gmail.com>
2021-03-31 13:53:44 +0100

    field cleared after alert box

--- js/weather.js
+++ js/weather.js
@@ -15,13 +15,19 @@ function showWeather(e){
         let countryValue = data.name;
         let tempValue = data.main.temp;
         let descValue = data.weather[0].description;
+        let humidValue = data.main.humidity;
+        let pressureValue = data.main.pressure;
+
         document.querySelector('.name').innerHTML = 'Country: ' +countryValue;
-        document.querySelector('.temp').innerHTML = 'Temp: ' +tempValue;
+        document.querySelector('.temp').innerHTML = 'Temperature: ' +tempValue;
         document.querySelector('.desc').innerHTML = 'Description: ' +descValue;
+        document.querySelector('.hum').innerHTML = 'Humidity: ' +humidValue;
+        document.querySelector('.pres').innerHTML = 'Pressure: ' +pressureValue;
 
  
     })
     .catch(err => alert('Wrong City name'))
+    document.querySelector('.input').value = "";
 
 }
 
